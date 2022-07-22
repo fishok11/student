047ba36ad1fe57124c5f662a308a1c0772cc5c49
fishok11 <svllsd811@gmail.com>
2022-07-22 15:20:40 +0300

    fetch

--- js/study.js
+++ js/study.js
@@ -17,29 +17,28 @@ const Courses = {
     "2": "Физика",
     "3": "Программирование"
 }
-
 // ********************* main.js *********************
 
-let students = [];
+// let students = [];
 
-students.push(
-    new Student({name: "Вася", surname: "Иванов", speciality: "1", age: 18, training: "1", courses: ["2"]})
-);
-students.push(
-    new Student({name: "Петя", surname: "Тузов", speciality: "2", age: 22, training: "2", courses: ["1", "2"]})
-);
-students.push(
-    new Student({name: "Петя", surname: "Соболев", speciality: "1", age: 20, training: "2", courses: ["1", "2", "3"]})
-);
-students.push(
-    new Student({name: "Дима", surname: "Тугушев", speciality: "3", age: 19, training: "1", courses: ["1", "3"]})
-);
-students.push(
-    new Student({name: "Сева", surname: "Ладыгин", speciality: "4", age: 18, training: "2", courses: ["1", "2"]})
-);
+// students.push(
+//     new Student({name: "Вася", surname: "Иванов", speciality: "1", age: 18, training: "1", courses: ["2"]})
+// );
+// students.push(
+//     new Student({name: "Петя", surname: "Тузов", speciality: "2", age: 22, training: "2", courses: ["1", "2"]})
+// );
+// students.push(
+//     new Student({name: "Петя", surname: "Соболев", speciality: "1", age: 20, training: "2", courses: ["1", "2", "3"]})
+// );
+// students.push(
+//     new Student({name: "Дима", surname: "Тугушев", speciality: "3", age: 19, training: "1", courses: ["1", "3"]})
+// );
+// students.push(
+//     new Student({name: "Сева", surname: "Ладыгин", speciality: "4", age: 18, training: "2", courses: ["1", "2"]})
+// );
 
 
-generateTable(students)
+generateTable()
 
 
 
@@ -68,8 +67,7 @@ for (let key in Specs) {
 }
 
 
-
-function generateTable(students) {
+function generateTable() {
     let table = document.querySelector(".student")
 
     let thead = document.querySelector("thead")
@@ -84,130 +82,134 @@ function generateTable(students) {
 
     table.append(thead)
 
+    fetch("http://localhost:3000/students")
+        .then(response => response.json())
+        .then((data) => {
+            let student = data[0]
 
-
-    let student = students[0]
-
-    let tr = document.createElement("tr"); 
-
-        for (let key in student) {
-            if (key !== "available") {
-                let th = document.createElement("th");
-
-                th.innerHTML = key
-   
-                tr.append(th); 
-            }
-        }
-
-    let thButton = document.createElement("th");
-    
-    thButton.innerHTML = "update/expel"
-
-    tr.append(thButton); 
-
-    thead.append(tr); 
-
-
-
-    let tbody = document.querySelector("tbody")
-
-        if (tbody) {
-            tbody.remove()
-        }
-
-    tbody = document.createElement("tbody");
-
-    tbody.classList.add("student-body")
-
-    table.append(tbody)
-    
-
-
-    for (let student of students) {
-        if (student.isAvailable() === true) {
             let tr = document.createElement("tr"); 
-
-            for (let column in student) {
-                if (column !== "available") {
-                    let td = document.createElement("td");
-
-                    if (column === "speciality") {
-                        td.innerHTML = Specs[student[column]]
-                    } else if (column === "training") {
-                        td.innerHTML = TypeOfTraining[student[column]]
-                    } else
-                     if (column === "courses") {           
-                        const arr = student[column]       
-                        td.innerHTML = arr.map(item => " " + Courses[item]);               
-                    } else {
-                        td.innerHTML = student[column] 
+        
+                for (let key in student) {
+                    if (key !== "available") {
+                        let th = document.createElement("th");
+        
+                        th.innerHTML = key
+           
+                        tr.append(th); 
                     }
-
-                    tr.append(td); 
                 }
+        
+            let thButton = document.createElement("th");
+            
+            thButton.innerHTML = "update/expel"
+        
+            tr.append(thButton); 
+        
+            thead.append(tr); 
+            
+
+            let tbody = document.querySelector("tbody")
+
+            if (tbody) {
+                tbody.remove()
             }
+        
+            tbody = document.createElement("tbody");
+        
+            tbody.classList.add("student-body")
+        
+            table.append(tbody)
+            
+        
+        
+            for (let student of data) {
+                
+                let tr = document.createElement("tr"); 
+
+                for (let column in student) {
+                    if (column !== "available") {
+                        let td = document.createElement("td");
+
+                        if (column === "speciality") {
+                            td.innerHTML = Specs[student[column]]
+                        } else if (column === "training") {
+                            td.innerHTML = TypeOfTraining[student[column]]
+                        } else
+                            if (column === "courses") {           
+                            const arr = student[column]       
+                            td.innerHTML = arr.map(item => " " + Courses[item]);               
+                        } else {
+                            td.innerHTML = student[column] 
+                        }
+
+                        tr.append(td); 
+                    }
+                }
 
 
 
-            let buttonUpdate = document.createElement("button"); 
+                let buttonUpdate = document.createElement("button"); 
 
-            let tdButton = document.createElement("td"); 
+                let tdButton = document.createElement("td"); 
 
-            buttonUpdate.classList.add("button-update")
+                buttonUpdate.classList.add("button-update")
 
-            buttonUpdate.innerHTML = "Изменить"
+                buttonUpdate.innerHTML = "Изменить"
 
-            buttonUpdate.onclick = function() {
-                const studentId = student.id;
-                const oneStudent = students.find(student => studentId === student.id)
-                const form = document.querySelector("[name = 'formUpdate']")
+                buttonUpdate.onclick = function() {
+                    const studentId = student.id;
+                    const oneStudent = data.find(student => studentId === student.id)
+                    const form = document.querySelector("[name = 'formUpdate']")
 
-                form.id.value = oneStudent.id
-                form.name.value = oneStudent.name
-                form.surname.value = oneStudent.surname
-                selectUpdate.value = oneStudent.speciality
-                form.age.value = oneStudent.age
-                form.training.value = oneStudent.training
-                document.querySelectorAll(".input-checkbox-update").forEach(checkbox => {
-                    checkbox.checked = false;
-                });
-                oneStudent.courses.forEach(course => {
-                    document.getElementById(course + "-checkbox-update").checked = true;
-                });
-            }
+                    form.id.value = oneStudent.id
+                    form.name.value = oneStudent.name
+                    form.surname.value = oneStudent.surname
+                    selectUpdate.value = oneStudent.speciality
+                    form.age.value = oneStudent.age
+                    form.training.value = oneStudent.training
+                    document.querySelectorAll(".input-checkbox-update").forEach(checkbox => {
+                        checkbox.checked = false;
+                    });
+                    oneStudent.courses.forEach(course => {
+                        document.getElementById(course + "-checkbox-update").checked = true;
+                    });
+                }
 
-            tdButton.append(buttonUpdate); 
+                tdButton.append(buttonUpdate); 
 
-            tr.append(tdButton); 
-
-
-
-            let buttonExpel = document.createElement("button"); 
-
-            buttonExpel.classList.add("button-expel")
-
-            buttonExpel.innerHTML = "Отчислить" 
-
-            buttonExpel.onclick = function() {
-                const studentId = student.id;
-                students.find(student => studentId === student.id).expel()
-                generateTable(students)
-            }
-
-            tdButton.append(buttonExpel); 
-
-            tr.append(tdButton); 
+                tr.append(tdButton); 
 
 
 
-            tbody.append(tr); 
+                let buttonExpel = document.createElement("button"); 
+
+                buttonExpel.classList.add("button-expel")
+
+                buttonExpel.innerHTML = "Отчислить" 
+
+                buttonExpel.onclick = function() {
+                    const studentId = student.id;
+
+                    fetch( `http://localhost:3000/students/` + studentId, {
+                        method: 'DELETE',
+                    });
+
+                }
+
+                tdButton.append(buttonExpel); 
+
+                tr.append(tdButton); 
+                
+
+                tbody.append(tr); 
+            
         }
-    }
+    });
 }
 
 document.querySelector(".button-add").addEventListener("click", function(e) {
     e.preventDefault()
+
 
     const form = document.querySelector("[name = 'formAdd']")
 
@@ -215,18 +217,24 @@ document.querySelector(".button-add").addEventListener("click", function(e) {
     let arr = [];
     checkboxes.forEach((checkbox) => {arr.push(checkbox.value)})
     
-    students.push(
-        new Student({ 
-            name: form.name.value, 
-            surname: form.surname.value, 
-            speciality: selectAdd.value, 
-            age: form.age.value, 
-            training: form.training.value,
-            courses: arr,
-        })
-    );
+    let user = { 
+        id:"",
+        name: form.name.value, 
+        surname: form.surname.value, 
+        speciality: selectAdd.value, 
+        age: form.age.value, 
+        training: form.training.value,
+        courses: arr,
+    }
 
-    generateTable(students)
+
+    fetch('http://localhost:3000/students', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+    });
 });
 
 document.querySelector(".button-save").addEventListener("click", function(e) {
@@ -241,14 +249,21 @@ document.querySelector(".button-save").addEventListener("click", function(e) {
         arr.push(checkbox.value)
     })
 
-    students.find(student => studentId === student.id).update({     
-        name: form.name.value,
-        surname: form.surname.value,
-        speciality: selectUpdate.value,
-        age: form.age.value,
+    let user = { 
+        id:"",
+        name: form.name.value, 
+        surname: form.surname.value, 
+        speciality: selectAdd.value, 
+        age: form.age.value, 
         training: form.training.value,
         courses: arr,
-    });
+    }
 
-    generateTable(students)
+    fetch('http://localhost:3000/students/' + studentId, {
+        method: 'PUT',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+    });
 });
